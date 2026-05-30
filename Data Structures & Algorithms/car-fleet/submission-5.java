2ab2f467c65f23254697f85159c55169b07add31
Dhanu bhardwaj <37215333+dhanu27@users.noreply.github.com>
2026-05-30 14:23:33 +0530

    Add: car-fleet - submission-5

---
+++ Data Structures & Algorithms/car-fleet/submission-5.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int carFleet(int target, int[] position, int[] speed) {
+        int n = position.length;
+        int[][] cars = new int[n][2];
+        for(int i=0; i<n; i++){
+            cars[i][0] = position[i];
+            cars[i][1] = speed[i]; 
+        }
+        Arrays.sort(cars, new Comparator<int[]>(){
+            @Override
+            public int compare(int[] arr1, int []arr2){
+             return arr2[0] - arr1[0];
+           }
+        });
+        Stack<Double> st = new Stack<>();
+        for(int i=0; i<n; i++){
+            Double time = ((double)(target - cars[i][0]) / cars[i][1]);
+                Double secondTime = time;
+                if(!st.isEmpty() && st.peek() >= time){
+                    secondTime = st.peek();
+                   st.pop();       
+                }
+                st.push(Math.max(secondTime,time));
+                
+        }
+        return st.size();  
+    }
+}
